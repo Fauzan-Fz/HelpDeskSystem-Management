ddafbd184a7933514e0f22caa4ab8504b8b7e0f2
Fauzan-F <fauzanfirdaus1966@gmail.com>
2025-02-22 12:33:52 +0700

    22/02/2025

---
+++ Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace HelpDeskSystem.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string TicketId { get; set; } // Changed from int to string
+        public Ticket Ticket { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedById { get; set; }
+        public ApplicationUser CreatedBy { get; set; }
+    }
+}

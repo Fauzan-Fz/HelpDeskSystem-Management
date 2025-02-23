779b3750c0ca34d9b343cfbc4e9e1888f324505c
Fauzan-F <fauzanfirdaus1966@gmail.com>
2025-02-23 13:18:22 +0700

    23/02/2025

---
+++ Controllers/UesrController.cs
@@ -0,0 +1,102 @@
+﻿using HelpDeskSystem.Data;
+using HelpDeskSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskSystem.Controllers
+{
+    public class UesrController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public UesrController(ApplicationDbContext context, UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _roleManager = roleManager;
+
+            _signInManager = signInManager;
+
+            _userManager = userManager;
+
+            _context = context;
+        }
+
+        // GET: UesrController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UesrController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UesrController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UesrController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UesrController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UesrController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UesrController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UesrController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

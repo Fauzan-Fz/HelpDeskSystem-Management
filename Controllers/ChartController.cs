d423dab563baf00ec804b4850ad234e9c608e8f9
Fauzan-F <fauzanfirdaus1966@gmail.com>
2025-03-25 17:59:24 +0700

    25/03/2025

--- Controllers/ChartController.cs
+++ Controllers/ChartController.cs
@@ -44,11 +44,78 @@ namespace HelpDeskSystem.Controllers
 
                     label = "Users",
                     data,
-                    borderWidth = 1
+                    borderWidth = 0
                 }
             }
             };
 
+            return Json(chartData);
+        }
+
+        [HttpGet]
+        public IActionResult GetCountryDataChart()
+        {
+            var cityCounts = _context.Users
+                .GroupBy(u => string.IsNullOrEmpty(u.Country) ? "Unknown" : u.Country)
+                .Select(g => new
+                {
+                    Country = g.Key,
+                    Count = g.Count() // Hitung jumlah user per country
+                })
+                .ToList();
+
+            var labels = cityCounts.Select(g => g.Country).ToArray();
+            var data = cityCounts.Select(g => g.Count).ToArray();
+
+            var chartData = new
+            {
+                labels,
+                datasets = new[]
+                {
+                    new
+                        {
+                        label = "",
+                        data,
+                        borderWidth = 0,
+                        backgroundColor = new[] { "#FF6384", "#36A2EB", "#FFCE56" }
+                        }
+                }
+
+            };
+            return Json(chartData);
+        }
+
+        [HttpGet]
+        public IActionResult GetTicketDataChart()
+        {
+            var ticketCounts = _context.Tickets
+                .GroupBy(u => string.IsNullOrEmpty(u.Title) ? "Unknown" : u.Title)
+                .Select(g => new
+                {
+                    Title = g.Key,
+                    Count = g.Count() // Hitung jumlah user per country
+                })
+                .ToList();
+
+            var labels = ticketCounts.Select(g => g.Title).ToArray();
+            var data = ticketCounts.Select(g => g.Count).ToArray();
+
+            var chartData = new
+            {
+                labels,
+                datasets = new[]
+                {
+            new
+            {
+                label = "Tickets",
+                data,
+                borderWidth = 2,  // Agar garis terlihat
+                borderColor = "#AC5947",  // Warna garis
+                backgroundColor = "rgba(172, 89, 71, 0.2)", // Warna fill bawah garis
+                fill = true
+            }
+                }
+            };
             return Json(chartData);
         }
     }

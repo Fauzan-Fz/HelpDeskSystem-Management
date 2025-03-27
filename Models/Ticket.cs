3f26494d43777cd305073ef91d83bd52991dfc18
Fauzan-F <fauzanfirdaus1966@gmail.com>
2025-03-27 21:09:59 +0700

    27/03/2025

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,14 +1,33 @@
-﻿namespace HelpDeskSystem.Models
+﻿using System.ComponentModel;
+
+namespace HelpDeskSystem.Models
 {
     public class Ticket
     {
+        [DisplayName("Id")]
         public string Id { get; set; }
+
+        [DisplayName("Title")]
         public string Title { get; set; }
+
+        [DisplayName("Description")]
         public string Description { get; set; }
+
+        [DisplayName("Status")]
         public string Status { get; set; }
+
+        [DisplayName("Priority")]
         public string Priority { get; set; }
+
+        [DisplayName("Created By")]
         public string CreatedById { get; set; }
         public ApplicationUser CreatedBy { get; set; }
+
+        [DisplayName("Created On")]
         public DateTime CreatedOn { get; set; }
+
+        [DisplayName("Ticket Category")]
+        public int? SubCategoryId { get; set; }
+        public TicketSubCategory SubCategory { get; set; }
     }
 }

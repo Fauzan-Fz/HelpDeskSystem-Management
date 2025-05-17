e1a7c74f5a36980116f29d89c2b27d1d6639d29a
Fauzan-F <fauzanfirdaus1966@gmail.com>
2025-05-17 17:32:24 +0700

    17/05/2025

---
+++ AuditsManager/AuditTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace HelpDeskSystem.AuditsManager
+{
+    public enum AuditTypes
+    {
+        None = 0,
+        Create = 1,
+        Update = 2,
+        Delete = 3,
+        Login = 4
+    }
+}
